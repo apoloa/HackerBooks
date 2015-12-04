e1062754bd2d82d8f61662f492017e7ff28f4738
Adrian <a.whole.dev@gmail.com>
2015-12-04 17:22:16 +0100

    Added Library and unit test to create the Library

---
+++ ReadBook/Library.swift
@@ -0,0 +1,47 @@
+//
+//  Library.swift
+//  ReadBook
+//
+//  Created by Adrian Polo Alcaide on 04/12/15.
+//  Copyright © 2015 Adrian. All rights reserved.
+//
+
+import UIKit
+
+
+class Library {
+    // MARK: - Variables
+    let books : [Book]
+    let tags : [String]
+    
+    // MARK - Constructor
+    
+    init (arrayOfBooks books: [Book], tags: [String]){
+        self.books = books
+        self.tags = tags
+    }
+}
+
+// This extension can help to create a Library. 
+extension Library {
+    
+    convenience init(arrayOfBooks books: [Book]){
+        let tags = Library.getAllTags(arrayOfBooks: books)
+        
+        self.init(arrayOfBooks:books, tags:tags)
+    }
+    
+    private static func getAllTags(arrayOfBooks books:[Book]) -> [String]{
+        var tags : [String] = []
+        for book in books{
+            for tag in book.tags{
+                if !tags.contains(tag){
+                    tags.append(tag)
+                    continue
+                }
+            }
+        }
+        return tags
+    }
+    
+}
---
+++ ReadBookTests/LibraryTest.swift
@@ -0,0 +1,50 @@
+//
+//  LibraryTest.swift
+//  ReadBook
+//
+//  Created by Adrian Polo Alcaide on 04/12/15.
+//  Copyright © 2015 Adrian. All rights reserved.
+//
+
+import XCTest
+
+@testable import ReadBook
+class LibraryTest: XCTestCase {
+
+    var books = [Book]()
+    var library : Library?
+    
+    override func setUp() {
+        var title = "PruebaBook"
+        var authors = ["A", "B"]
+        var tags = ["tags", "algorithms"]
+        var image = NSURL(string: "http://google.es")
+        var url = NSURL(string: "http://www.amoma.com/hotel.php?key=20151207201512081205000000500000050000111115235&id=127753&referer=06658traHty5C_esB9&curid=EUR&langid=4&sessionKey=LSEtRGUxNFFvQTkyQXIxR29lNUxiTWZLWFZTbmtHYy9UQmUxbDVXbmVvVllYdThFQ1FPd2kxUkRsSWozM0twTVBtejVPN09iWVNqTG5INThYMVMwL1BQMnc9PQ%3D%3D&reqsource=2&clickThroughId=386281683")
+        
+        var book = Book(title: title, authors: authors, tags: tags, image: image, url: url)
+        
+        books.append(book)
+        
+        authors = ["Scott Chacon", "Ben Straub"]
+        image = NSURL(string: "http://hackershelf.com/media/cache/b4/24/b42409de128aa7f1c9abbbfa549914de.jpg")
+        url = NSURL(string:"https://progit2.s3.amazonaws.com/en/2015-03-06-439c2/progit-en.376.pdf")
+        tags = ["version control","git"]
+        title = "Pro Git"
+        
+        book = Book(title: title, authors: authors, tags: tags, image: image, url: url)
+        
+        books.append(book)
+    }
+    
+    func testCreateLibrary() {
+        
+        library = Library(arrayOfBooks: books)
+        XCTAssertNotNil(library)
+        
+    }
+    
+    
+
+    
+
+}
