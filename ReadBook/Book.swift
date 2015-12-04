55b059a4f6bbf6249033876c94b2b4d75d9671d1
Adrian <a.whole.dev@gmail.com>
2015-12-04 02:04:37 +0100

    Modified Book class with specifications

--- ReadBook/Book.swift
+++ ReadBook/Book.swift
@@ -5,5 +5,27 @@
 //  Created by Adrian Polo Alcaide on 03/12/15.
 //  Copyright © 2015 Adrian. All rights reserved.
 //
+//  This class represents a book
+//
 
-import Foundation
+import UIKit
+
+class Book{
+    
+    // MARK: - Attributes
+    let title : String
+    let authors : [String]
+    let tags : [String]
+    let image: NSURL?
+    let url: NSURL?
+    
+    
+    init(title: String, authors: [String], tags: [String], image: NSURL?, url: NSURL?){
+        self.title = title
+        self.authors = authors
+        self.tags = tags
+        self.image = image
+        self.url = url
+    }
+}
+
