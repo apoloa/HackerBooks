d8e65250d3be07b6ceaef593bff3cc67c09e7c2d
Adrian <a.whole.dev@gmail.com>
2015-12-04 02:02:55 +0100

    Created Book class and test

---
+++ ReadBook/Book.swift
@@ -0,0 +1,9 @@
+//
+//  Book.swift
+//  ReadBook
+//
+//  Created by Adrian Polo Alcaide on 03/12/15.
+//  Copyright © 2015 Adrian. All rights reserved.
+//
+
+import Foundation
---
+++ ReadBook/Tags.swift
@@ -0,0 +1,9 @@
+//
+//  Tags.swift
+//  ReadBook
+//
+//  Created by Adrian Polo Alcaide on 04/12/15.
+//  Copyright © 2015 Adrian. All rights reserved.
+//
+
+import Foundation
